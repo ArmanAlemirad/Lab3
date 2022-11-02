26ce25ce3e00cd20e615115a2b67272389ce2ea0
Stud <arman.alemirad@gmail.com>
2022-11-02 12:06:57 +0100

    View done

---
+++ src/main/java/com/example/drawing/DrawingController.java
@@ -0,0 +1,20 @@
+package com.example.drawing;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+
+public class DrawingController {
+    public ChoiceBox shapeList;
+    public ColorPicker colorPicker;
+
+
+    public Button undo;
+    public Button save;
+    public Canvas canvas;
+
+
+}
