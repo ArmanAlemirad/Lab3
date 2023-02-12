39559168b4b6a4f1366cea2cd9792aa35fbbb441
Ye4y <Arman.Alemirad@gmail.com>
2023-02-12 21:19:24 +0100

    sonar covrage

--- src/test/java/com/example/drawing/Controller/DrawingControllerTest.java
+++ src/test/java/com/example/drawing/Controller/DrawingControllerTest.java
@@ -49,4 +49,12 @@ class DrawingControllerTest {
         Assertions.assertEquals(315-234, rect.getHeight());
     }
 
+    @Test
+    void test4(){
+        Rectangle rect = new Rectangle(234,234,0, Color.BLACK,0,0);
+        Assertions.assertEquals(0, rect.getHeight());
+        drawingController.calculateHeight(315,rect);
+        Assertions.assertEquals(315-234, rect.getHeight());
+    }
+
 }
